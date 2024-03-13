b29b1bb158910d9968c133af5fa0168e35e5d959
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 12:29:58 -0600

    Update herramientas-topdf.js

--- plugins/herramientas-topdf.js
+++ plugins/herramientas-topdf.js
@@ -2,7 +2,7 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*[❗] 𝙰𝚗𝚜𝚠𝚎𝚛 / 𝚕𝚊𝚋𝚎𝚕 𝚊𝚗 𝚒𝚖𝚊𝚐𝚎*'    
+if (!mime) throw '*[❗] responder / etiquetar una imagen *'    
 let img = await q.download?.()
 let url = await uploadImage(img)    
 let docname = text ? text : m.pushName || 'documento'
