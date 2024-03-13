b39d05b2351f7b6373eb53760f0562bfd9b9bde4
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 12:23:04 -0600

    Update herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -4,14 +4,14 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `[❗] send an image and then send an image and then place the command ${usedPrefix + command}`;
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `[❗] the file format (${mime}) it was not possible to send tag to an image `;
-  m.reply("[❗] processing");
+  if (!mime) throw `[❗] enviar una imagen y luego enviar una imagen y luego colocar el comando ${usedPrefix + command}`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `[❗] the file format (${mime}) no fue posible enviar etiqueta a una imagen `;
+  m.reply("[❗] Procesando...");
   let img = await q.download?.();
   let pr = await remini(img, "enhance");
   conn.sendMessage(m.chat, {image: pr}, {quoted: m});
  } catch {
-  throw "❌an error occurred try again❌";
+  throw "❌Se produjo un error, intente nuevamente❌";
  }
 };
 handler.help = ["remini", "hd", "enhance"];
