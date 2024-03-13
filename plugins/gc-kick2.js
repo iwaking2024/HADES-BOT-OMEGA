92e56dc2e29cf9fa5b1122aa3d637341cec70be8
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 11:46:12 -0600

    Update gc-kick2.js

--- plugins/gc-kick2.js
+++ plugins/gc-kick2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] The owner does not have the restrictions enabled (#Enable Restrict) Contact the so that it enables it.*'
-let kicktext = `*[❗] Tag a person or respond to a group message to delete the user*\n\n*—◉ Example:*\n*${usedPrefix + command} @${global.suittag}*`
+if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] El propietario no tiene habilitadas las restricciones. (#Enable Restrict) Contacta con el para que lo habilite.*'
+let kicktext = `*[❗] Etiqueta a una persona o responde a un mensaje grupal para eliminar al usuario*\n\n*—◉ Example:*\n*${usedPrefix + command} @${global.suittag}*`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, { mentions: conn.parseMention(kicktext)}) 
 if (m.mentionedJid.includes(conn.user.jid)) return  
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
