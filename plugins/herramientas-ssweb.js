259aa04548f22061c4a6355e4367d860e06bde7f
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 12:28:27 -0600

    Update herramientas-ssweb.js

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '[🔎] enter the link of a page', m)
+if (!args[0]) return conn.reply(m.chat, '[🔎] ingresa al enlace de una pagina', m)
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
