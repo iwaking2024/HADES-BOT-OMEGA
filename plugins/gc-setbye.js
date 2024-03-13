e04fd68a090287f2a2712e9ddf65414d6680e41b
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 11:58:33 -0600

    Update gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sBye = text
-m.reply('[❗]GOODBYE MESSAGE CONFIGURED CORRECTLY FOR THIS GROUP ')
-} else throw `[❗]ENTER THE FAREWELL MESSAGE YOU WANT TO ADD USE: \n- @user (mención)`
+m.reply('[❗]MENSAJE DE ADIÓS CONFIGURADO CORRECTAMENTE PARA ESTE GRUPO ')
+} else throw `[❗]INGRESA EL MENSAJE DE DESPEDIDA QUE QUIERES AGREGAR UTILIZAR: \n- @user (mención)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
