934c442fa48e02b65a5534325b836eac26ce47dd
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 12:21:49 -0600

    Update herramientas-dallE.js

--- plugins/herramientas-dallE.js
+++ plugins/herramientas-dallE.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] Enter a text to create an image and thus use the Dall-E function*\n\n*—◉ Examples of requests*\n*◉ ${usedPrefix + command} Kittens crying*\n*◉ ${usedPrefix + command} hatsune miku kiss*`
+if (!text) throw `*[❗]Introduce un texto para crear una imagen y así utilizar la función Dall-E*\n\n*—◉ Ejemplos de peticiones*\n*◉ ${usedPrefix + command} Kittens crying*\n*◉ ${usedPrefix + command} hatsune miku kiss*`
 try {
-m.reply('*[❗] Wait for a moment in what the order commanding*')
+m.reply('*[❗] Espere un momento en lo que ordena*')
 let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
 await conn.sendFile(m.chat, tiores.data, null, null, m)
 } catch {
-throw `*[❗] Error, please try again*`
+throw `*[❗] Error, inténtalo de nuevo*`
 }}
 handler.command = ['dall-e', 'dalle', 'ia2', 'cimg', 'openai3']
 handler.register = true
