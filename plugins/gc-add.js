8bde499522507a3ec7182eac07b59002a32851bd
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-13 11:28:09 -0600

    Update gc-add.js

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -2,8 +2,8 @@ let { generateWAMessageFromContent, prepareWAMessageMedia, proto } = (await impo
 import fetch from 'node-fetch'
 const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@whiskeysockets/baileys')).default
 let handler = async (m, { conn, text, participants, args }) => {  
-if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] The owner does not have the restrictions enabled (#Enable Restrict) Contact the so that it enables it.*'
-if (!args[0]) throw '*[❗] Enter the user number you want to add*'    
+if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] El propietario no tiene habilitadas las restricciones (#Enable Restrict) Contactar con el para que lo habilite.*'
+if (!args[0]) throw '*[❗] Ingrese el número de usuario que desea agregar*'    
 try {    
 let _participants = participants.map(user => user.id)
 let users = (await Promise.all(
@@ -21,14 +21,14 @@ const jid = user.attrs.jid
 const content = getBinaryNodeChild(user, 'add_request')
 const invite_code = content.attrs.code
 const invite_code_exp = content.attrs.expiration
-let teks = `[?] It was not possible to add @${jid.split('@')[0]}, This can happen because the number is incorrect, the person has recently left the group or the person has set his/her privacy in groups, the group invitation in his/her privacy has been sent to the user*`
+let teks = `[?] No fue posible agregar @${jid.split('@')[0]}, Esto puede suceder porque el número es incorrecto, la persona ha abandonado recientemente el grupo o la persona ha configurado su privacidad en grupos, la invitación del grupo en su privacidad ha sido enviada al usuario.*`
 m.reply(teks, null, { mentions: conn.parseMention(teks)})
-let captionn = `Hey!! Hi, I introduce myself, I'm Hades-Bot-md, and I am a Bot for Whatsapp, a group of the group use the command to add to the group, but I could not add you, so I'll send you the invitation so that you add you, we wait you!!`    
+let captionn = `¡¡Ey!! Hola, me presento, soy el BOT de Whatsapp y un buen grupo uso el comando para agregarte al grupo, pero no pude agregarte, así que te enviaré la invitación para que te agregues, te esperamos!!`    
 var messaa = await prepareWAMessageMedia({ image: jpegThumbnail }, { upload: conn.waUploadToServer })
 var groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({ groupInviteMessage: { groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail }}), { userJid: jid })
 await conn.relayMessage(jid, groupInvite.message, { messageId: groupInvite.key.id })}
 } catch {
-throw '*[??] It was not possible to add to the number entered, this can happen because the number is incorrect, the person has recently left the group or has set the group privacy. We advise you to send the invitation manually!!*'
+throw '*[??] No fue posible agregar al número ingresado, esto puede suceder porque el número es incorrecto, la persona abandonó recientemente el grupo o configuró la privacidad del grupo. Te recomendamos enviar la invitación manualmente.!!*'
 }}
 handler.help = ['add', '+'].map(v => v + ' número')
 handler.tags = ['group']
